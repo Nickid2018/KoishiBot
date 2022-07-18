772286b6273ac869aa9172cef66a658d62603a80
Nickid2018 <nickid2018@outlook.com>
2022-07-18 18:40:29 +0800

    Multiline support

--- src/main/java/io/github/nickid2018/koishibot/resolver/MCChatListen.java
+++ src/main/java/io/github/nickid2018/koishibot/resolver/MCChatListen.java
@@ -3,6 +3,7 @@ package io.github.nickid2018.koishibot.resolver;
 import io.github.nickid2018.koishibot.mc.chat.MCChatBridge;
 import io.github.nickid2018.koishibot.message.api.Environment;
 import io.github.nickid2018.koishibot.message.api.MessageContext;
+import io.github.nickid2018.koishibot.util.AsyncUtil;
 import kotlin.Pair;
 
 public class MCChatListen extends MessageResolver {
@@ -18,7 +19,7 @@ public class MCChatListen extends MessageResolver {
 
     @Override
     public boolean resolveInternal(String key, MessageContext context, Object resolvedArguments, Environment environment) {
-        MCChatBridge.INSTANCE.onReceiveGroupText(context.group(), context.user(), key);
+        AsyncUtil.execute(() -> MCChatBridge.INSTANCE.onReceiveGroupText(context.group(), context.user(), key));
         return false;
     }
 }
