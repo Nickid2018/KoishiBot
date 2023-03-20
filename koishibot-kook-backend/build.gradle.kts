48e517ce4915ff3c0dc237dc28a10a4a8c4d8230
Nickid2018 <nickid2018@outlook.com>
2023-03-20 10:37:27 +0800

    kook backend (start)

---
+++ koishibot-kook-backend/build.gradle.kts
@@ -0,0 +1,71 @@
+import java.security.MessageDigest
+
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+dependencies {
+    api(project(":koishibot-message-api"))
+
+    api("com.github.KookyBot:KookyBot:0.2.4")
+
+    // Logging
+    api("org.apache.logging.log4j:log4j-api:2.19.0")
+    api("org.apache.logging.log4j:log4j-core:2.19.0")
+    api("org.apache.logging.log4j:log4j-slf4j-impl:2.19.0")
+}
+
+tasks {
+    jar {
+        manifest.attributes["Main-Class"] = "io.github.nickid2018.koishibot.backend.Main"
+        manifest.attributes["Class-Path"] = configurations.runtimeClasspath.get()
+            .map { it.name }
+            .filter { it.endsWith(".jar") }
+            .sorted()
+            .joinToString(" ") { "libraries/$it" }
+    }
+
+    register<Sync>("exportApi") {
+        from(configurations.runtimeClasspath)
+        into(layout.buildDirectory.dir("apis"))
+    }
+
+    register("computeChecksum") {
+        doLast {
+            val md = MessageDigest.getInstance("SHA-256")
+
+            layout.buildDirectory.dir("apis").get().asFileTree.files
+                .filter { it.isFile }
+                .filter { !it.name.contains("koishibot") }
+                .sortedBy { it.name }
+                .forEach { md.update(it.readBytes()) }
+            parent!!.layout.projectDirectory.dir("koishibot-message-api/src/main/java").asFileTree.files
+                .filter { it.isFile }
+                .sortedBy { it.name }
+                .forEach { md.update(it.readBytes()) }
+            parent!!.layout.projectDirectory.dir("koishibot-network/src/main/java").asFileTree.files
+                .filter { it.isFile }
+                .sortedBy { it.name }
+                .forEach { md.update(it.readBytes()) }
+            val checksumAPI = md.digest().joinToString("") { "%02x".format(it) }
+            layout.projectDirectory.dir("src/main/java").asFileTree.files
+                .filter { it.isFile }
+                .sortedBy { it.name }
+                .forEach { md.update(it.readBytes()) }
+            val checksumJar = md.digest().joinToString("") { "%02x".format(it) }
+
+            parent!!.layout.buildDirectory.file("libs/kook-backend-checksum.txt").get().asFile.writeText(
+                "$checksumJar\n$checksumAPI"
+            )
+        }
+    }
+}
+
+tasks["exportApi"].dependsOn("jar")
+tasks["computeChecksum"].dependsOn("exportApi")
+tasks["build"].dependsOn("computeChecksum")
---
+++ koishibot-kook-backend/src/main/java/io/github/nickid2018/koishibot/backend/Main.java
@@ -0,0 +1,8 @@
+package io.github.nickid2018.koishibot.backend;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
